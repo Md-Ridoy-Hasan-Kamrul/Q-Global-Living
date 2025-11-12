640d4daaa9d1daefdb56c0785dbfb1453552d8b6
shanjid5566 <shanjidahmed66@gmail.com>
2025-11-12 10:36:38 +0600

    Policy policy translate added

--- src/app/[locale]/privacy-policy/components/MainContent.jsx
+++ src/app/[locale]/privacy-policy/components/MainContent.jsx
@@ -1,21 +1,24 @@
+import { useLanguage } from "@/contexts/LanguageContext";
+import { useTranslation } from "@/i18n";
 import React from "react";
 
 const MainContent = ({ privacy, items }) => {
+  const { locale } = useLanguage();
+  const { t } = useTranslation(locale);
   return (
     <main className="w-full lg:w-3/4 space-y-6 lg:mt-0 md:mt-12 sm:mt-10 mt-8">
       <header>
         <h2 className="text-3xl sm:text-4xl lg:text-5xl font-bold tracking-tight mb-3">
-          Privacy Policy
+          {t("Policy.MainContent.title", "Privacy Policy")}
         </h2>
         <p className="text-sm text-charcoal/80 dark:text-soft-grey/80 mb-4">
-          Last Updated: October 26, 2023
+          {t(
+            "Policy.MainContent.lastUpdated",
+            "Last Updated: October 26, 2023"
+          )}
         </p>
         <p className="font-normal text-base sm:text-base text-charcoal/90 dark:text-soft-grey/90">
-          At Q Homes, we are committed to protecting your privacy and ensuring
-          the security of your personal information. This Privacy Policy
-          outlines how we collect, use, share, and protect your data when you
-          use our services. By accessing or using our platform, you agree to the
-          terms of this Privacy Policy.
+          {t("Policy.MainContent.subtitle")}
         </p>
       </header>
 
